d2fadbc0dc69c3ee3b94a2450f1776ac8dfe81fe
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-08-22 20:07:54 -0300

    desktop finalizado

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded",function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector(".hero");
 
-    buttons.forEach((btn)=>{
-        btn.addEventListener("click",function(e){
+    window.addEventListener("scroll", function (e) {
+        if (window.scrollY < heroSection.clientHeight) {
+            document.querySelector(".header").classList.add("header--is-hidden");
+        } else {
+            document.querySelector(".header").classList.remove("header--is-hidden");
+        }
+    })
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", function (e) {
             // console.log(e.target.getAttribute('data-tab-button'));
             // console.log(e.target.dataset.tabButton);
             const abaAlvo = e.target.dataset.tabButton;
@@ -12,18 +22,24 @@ document.addEventListener("DOMContentLoaded",function(e){
             e.target.classList.add("shows__tabs__button--is-active");
         })
     })
+
+    questions.forEach((question) => {
+        question.addEventListener("click", function (e) {
+            question.parentNode.classList.toggle("faq__questions__item--is-open")
+        })
+    })
 })
 
 function removeBordaBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    buttons.forEach((btn)=>{
+    buttons.forEach((btn) => {
         btn.classList.remove("shows__tabs__button--is-active");
     })
 }
 
 function escondeAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
-    tabsContainer.forEach((tab)=>{
+    tabsContainer.forEach((tab) => {
         tab.classList.remove("shows__list--is-active")
     })
 }
